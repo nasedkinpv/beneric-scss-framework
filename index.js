ea326859a5234115a10474dda3e48e723ec9cc61
FS <nasedkin@me.com>
2021-09-01 11:08:20 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Beneric SCSS Framework
+ * version: 1.0.0
+ *
+ * Contains:
+ * ~@drewbot/sass-flexbox-grid/public/sass-flexbox/scss/main.scss
+ * ~modularscale-sass/stylesheets/modularscale
+ * sass-mq
+ *
+ */
+
+var sass = require("sass");
+var Fiber = require("fibers");
+
+var sass = require('sass'); // or require('node-sass');
+
+var result = sass.renderSync({
+    file: "input.scss", outputStyle: "expanded", sourceMap: true,
+    outFile: "out.css"
+});
+console.log(sass.info);
+console.log(result);
+ // ...
