4c95247b07b7274c0e4e9bb78cb96a9026304e1a
Ben Nasedkin <ben@nasedk.in>
2024-05-25 17:45:52 +0300

    updated

--- index.js
+++ index.js
@@ -11,14 +11,21 @@
  */
 
 var sass = require("sass");
-var Fiber = require("fibers");
+// var Fiber = require("fibers");
+const fs = require('fs');
+const mergeSass = require('merge-sass');
 
-var sass = require('sass'); // or require('node-sass');
-
+const merge = mergeSass(
+    fs.readFileSync('modularscale-sass/_modularscale.scss'),
+    fs.readFileSync('sass-flexbox/scss/main.scss'),
+    fs.readFileSync('sass-mq/_mq.scss'),
+    fs.readFileSync('utilities/index.scss'),
+);
 var result = sass.renderSync({
     file: "index.scss", outputStyle: "expanded", sourceMap: true,
     outFile: "out.css"
 });
 console.log(sass.info);
+console.log(merge);
 console.log(result);
  // ...
